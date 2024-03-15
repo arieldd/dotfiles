7b535cc1a004598254a4c64cab63d9f590c57526
Ariel Diaz De Armas <arieldd@amazon.co.uk>
2024-03-15 15:47:57 +0000

    Added color scheme and /after.remaps

--- lua/arieldd/init.lua
+++ lua/arieldd/init.lua
@@ -1,4 +1,6 @@
 require("arieldd.remap")
 require("arieldd.lazy-setup")
-
 require("lazy").setup("arieldd.plugins")
+
+vim.o.background = "dark"
+vim.cmd.colorscheme "gruvbox"
---
+++ lua/arieldd/plugins/color-scheme.lua
@@ -0,0 +1,6 @@
+return{ 
+	{"catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{"rose-pine/neovim", name = "rose-pine" },
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
+}
+
