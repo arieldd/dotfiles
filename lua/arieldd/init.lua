293157f7424c5a4390bb5ec2c5d4e43ccf1eef28
Ariel Diaz <appa232@gmail.com>
2024-03-16 19:51:25 +0000

    Added nvim-tree + disable harpoon for now

--- lua/arieldd/init.lua
+++ lua/arieldd/init.lua
@@ -3,4 +3,7 @@ require("arieldd.set")
 require("arieldd.lazy-setup")
 require("lazy").setup("arieldd.plugins")
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.cmd.colorscheme "rose-pine"
