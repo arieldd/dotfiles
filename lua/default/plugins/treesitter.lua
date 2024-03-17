0f6817f4b3bd4c22923dae640ce3a3ca7e42e71f
Ariel Diaz <appa232@gmail.com>
2024-03-17 19:18:49 +0000

    Kickstart LSP config and plugins

--- lua/default/plugins/treesitter.lua
+++ lua/default/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
-				ensure_installed = { "c", "lua", "rust", "javascript", "html", "cpp", "go", "python", "swift", "java" , "query" , "vimdoc" },
+				ensure_installed = { "c", "lua", "rust", "javascript", "html", "cpp", "go", "python", "swift", "java" , "query" , "vimdoc", "markdown" },
 				sync_install = false,
 				highlight = { enable = true },
 				indent = { enable = true },
