529a41effd0b869849e47245c563fe6b9b4e4155
Ariel Diaz <appa232@gmail.com>
2024-03-18 22:03:19 +0000

    Added formatters and neotest.

--- lua/default/plugins/lsp-setup.lua
+++ lua/default/plugins/lsp-setup.lua
@@ -62,6 +62,8 @@ return {
             local ensure_installed = vim.tbl_keys(servers or {})
             vim.list_extend(ensure_installed, {
                 'stylua', -- Used to format lua code
+                'clang-format',
+                'prettier',
             })
             require('mason-tool-installer').setup { ensure_installed = ensure_installed }
             require('mason-lspconfig').setup {
