303646677af8f29b6391c0bf5fcc9a4f3cd1f29c
Ariel Diaz <appa232@gmail.com>
2024-03-19 00:42:03 +0000

    Added bordered completion windows.

--- lua/default/plugins/completions.lua
+++ lua/default/plugins/completions.lua
@@ -12,9 +12,9 @@ return {
       end)(),
       dependencies = {
         {
-          'rafamadriz/friendly-snippets',
+          "rafamadriz/friendly-snippets",
           config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
+            require("luasnip.loaders.from_vscode").lazy_load()
           end,
         },
       },
@@ -34,26 +34,21 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
+
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+
       completion = { completeopt = "menu,menuone,noinsert" },
 
       mapping = cmp.mapping.preset.insert({
-        -- Select the [n]ext item
         ["<C-n>"] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
         ["<C-p>"] = cmp.mapping.select_prev_item(),
-
-        -- scroll the documentation window [b]ack / [f]orward
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-        -- Manually trigger a completion from nvim-cmp.
-        --  Generally you don't need this, because nvim-cmp will display
-        --  completions whenever it has completion options available.
         ["<C-Space>"] = cmp.mapping.complete({}),
 
         -- Think of <c-l> as moving to the right of your snippet expansion.
