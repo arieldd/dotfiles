13f64f869ad3f42d050ae40a74ea4d5ad1748970
Ariel Diaz De Armas <arieldd@amazon.co.uk>
2024-03-20 11:31:55 +0000

    Terminal ui touch up and yank autocommand.

--- lua/default/plugins/ui-configs/noice.lua
+++ lua/default/plugins/ui-configs/noice.lua
@@ -12,27 +12,20 @@ require("noice").setup({
 	cmdline = {
 		enabled = true, -- enables the Noice cmdline UI
 		view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-		opts = {}, -- global options for the cmdline. See section on views
-		---@type table<string, CmdlineFormat>
+		opts = {
+			border = { padding = { 0 } },
+        },
 		format = {
-			-- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-			-- view: (default is cmdline view)
-			-- opts: any options passed to the view
-			-- icon_hl_group: optional hl_group for the icon
-			-- title: set to anything or empty string to hide
 			cmdline = { pattern = "^:", icon = " 󰘳 ", lang = "vim" },
-			search_down = { kind = "search", pattern = "^/", icon = "  ", lang = "regex", title = " Search " },
-			search_up = { kind = "search", pattern = "^%?", icon = "  ", lang = "regex", title = " Search " },
+			search_down = { kind = "search", pattern = "^/", icon = "   ", lang = "regex", title = " Search " },
+			search_up = { kind = "search", pattern = "^%?", icon = "   ", lang = "regex", title = " Search " },
 			filter = { pattern = "^:%s*!", icon = "  ", lang = "bash", title = " Shell " },
 			lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "  ", lang = "lua" },
-			help = { pattern = "^:%s*he?l?p?%s+", icon = " ", title = " Help " },
+			help = { pattern = "^:%s*he?l?p?%s+", icon = " ", title = " Help " },
 			input = {}, -- Used by input()
-			-- lua = false, -- to disable a format, set to `false`
 		},
 	},
 	messages = {
-		-- NOTE: If you enable messages, then the cmdline is enabled automatically.
-		-- This is a current Neovim limitation.
 		enabled = true, -- enables the Noice messages UI
 		view = "mini", -- default view for messages
 		view_error = "notify", -- view for errors
