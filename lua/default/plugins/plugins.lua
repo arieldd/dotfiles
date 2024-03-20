85fab81758081cc817017ec1cf5b1d203158dca6
Ariel Diaz De Armas <appa232@gmail.com>
2024-03-20 22:49:00 +0000

    Added proper Lualine options.

--- lua/default/plugins/plugins.lua
+++ lua/default/plugins/plugins.lua
@@ -1,78 +1,82 @@
 -- Collections of small non too config heavy plugins
 return {
-    { -- Buffer tabs on top
-        "akinsho/bufferline.nvim",
-        version = "*",
-        dependencies = "nvim-tree/nvim-web-devicons",
-        config = function()
-            require("bufferline").setup({})
-        end,
-    },
-    { -- Status line below
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        options = { theme = "wombat" },
-        config = function()
-            require("lualine").setup({})
-        end,
-    },
-    { -- !! Great for showing whitespaces and indents
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {},
-    },
-    { -- Mini
-        "echasnovski/mini.nvim",
-        config = function()
-            -- Better Around/Inside textobjects
-            --
-            -- Examples:
-            --  - va)  - [V]isually select [A]round [)]paren
-            --  - yinq - [Y]ank [I]nside [N]ext [']quote
-            --  - ci'  - [C]hange [I]nside [']quote
-            require("mini.ai").setup({ n_lines = 500 })
+	{ -- Buffer tabs on top
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({})
+		end,
+	},
+	{ -- Status line below
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = { theme = "gruvbox" },
+				extensions = {
+					"neo-tree",
+				},
+			})
+		end,
+	},
+	{ -- !! Great for showing whitespaces and indents
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
+	{ -- Mini
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [']quote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({ n_lines = 500 })
 
-            -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            --
-            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-            -- - sd'   - [S]urround [D]elete [']quotes
-            -- - sr)'  - [S]urround [R]eplace [)] [']
-            require("mini.surround").setup()
-        end,
-    },
-    { -- Toggle comments and such
-        "numToStr/Comment.nvim",
-        opts = {},
-        config = function()
-            require("Comment").setup()
-        end,
-    },
-    {
-        "windwp/nvim-autopairs",
-        enabled = false,
-        config = function()
-            require("nvim-autopairs").setup({
-                disable_filetype = { "TelescopePrompt", "vim" },
-            })
-        end,
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end,
-    },
-    { -- Undo tree for undo visualisation
-        "mbbill/undotree",
-        config = function()
-            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undo tree" })
-        end,
-    },
-    { -- Vim training game
-        "ThePrimeagen/vim-be-good",
-    },
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup()
+		end,
+	},
+	{ -- Toggle comments and such
+		"numToStr/Comment.nvim",
+		opts = {},
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		enabled = false,
+		config = function()
+			require("nvim-autopairs").setup({
+				disable_filetype = { "TelescopePrompt", "vim" },
+			})
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{ -- Undo tree for undo visualisation
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undo tree" })
+		end,
+	},
+	{ -- Vim training game
+		"ThePrimeagen/vim-be-good",
+	},
 }
