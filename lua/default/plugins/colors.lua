77c7307bc92d95bdb6342f01eb2dc8feb3dfa9eb
Ariel Diaz <appa232@gmail.com>
2024-03-20 00:59:02 +0000

    Went Gruvbox

--- lua/default/plugins/colors.lua
+++ lua/default/plugins/colors.lua
@@ -1,21 +1,37 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        opts = {
-            integrations = {
-                telescope = true,
-                harpoon = true,
-                mason = true,
-                neotest = true,
-            },
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.cmd.colorscheme("catppuccin")
-        end,
-    },
-    { "rose-pine/neovim",         name = "rose-pine" },
-    { "ellisonleao/gruvbox.nvim", priority = 1000,   config = true, opts = ... },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		-- opts = {
+		-- 	integrations = {
+		-- 		telescope = true,
+		-- 		harpoon = true,
+		-- 		mason = true,
+		-- 		neotest = true,
+		-- 	},
+		-- },
+		-- config = function(_, opts)
+		-- 	require("catppuccin").setup(opts)
+		-- 	vim.cmd.colorscheme("catppuccin")
+		-- end,
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+
+		opts = {
+			integrations = {
+				telescope = true,
+				harpoon = true,
+				mason = true,
+				neotest = true,
+			},
+		},
+		config = function(_, opts)
+			require("gruvbox").setup(opts)
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
 }
