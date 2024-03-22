4d4a902ea5b71904c952d7485aa00720e032d16c
Ariel Diaz <appa232@gmail.com>
2024-03-22 22:13:14 +0000

    Added JS, HTML LSP.

--- lua/default/plugins/lsp-setup.lua
+++ lua/default/plugins/lsp-setup.lua
@@ -71,7 +71,7 @@ return {
 			vim.list_extend(ensure_installed, {
 				"stylua",
 				"clang-format",
-				"prettier",
+				"prettierd",
 				"eslint_d",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
