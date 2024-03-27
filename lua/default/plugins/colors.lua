d0d0744f63e27a6cfd67b6493d3b1677107200b4
Ariel Diaz <appa232@gmail.com>
2024-03-27 22:32:50 +0000

    Some 'colors.lua' refactoring + trying out 'gruvbox-material'.

--- lua/default/plugins/colors.lua
+++ lua/default/plugins/colors.lua
@@ -1,35 +1,34 @@
-return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		-- priority = 1000,
-		-- opts = {
-		-- 	integrations = {
-		-- 		telescope = true,
-		-- 		mason = true,
-		-- 		neotest = true,
-		-- 	},
-		-- },
-		-- config = function(_, opts)
-		-- 	require("catppuccin").setup(opts)
-		-- 	vim.cmd.colorscheme("catppuccin")
-		-- end,
-	},
-	{ "rose-pine/neovim", name = "rose-pine" },
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		opts = {
-			integrations = {
-				telescope = true,
-				mason = true,
-				neotest = true,
-				lualine = true,
-			},
-		},
-		config = function(_, opts)
-			require("gruvbox").setup(opts)
-			vim.cmd.colorscheme("gruvbox")
-		end,
-	},
+local color_themes = {
+	["catppuccin"] = { "catppuccin/nvim" },
+	["rose-pine"] = { "rose-pine/neovim" },
+	["gruvbox"] = { "ellisonleao/gruvbox.nvim" },
+	["gruvbox-material"] = { "f4z3r/gruvbox-material.nvim" },
 }
+
+local function setTheme(theme_name)
+	local theme = color_themes[theme_name]
+
+	theme.name = theme_name
+	theme.priority = 1000
+	theme.opts = {
+		integrations = {
+			telescope = true,
+			mason = true,
+			neotest = true,
+		},
+	}
+	theme.config = function(_, opts)
+		require(theme_name).setup(opts)
+		vim.cmd.colorscheme(theme_name)
+	end
+end
+
+setTheme("gruvbox-material")
+
+local plugins = {}
+
+for _, v in pairs(color_themes) do
+	table.insert(plugins, v)
+end
+
+return plugins
