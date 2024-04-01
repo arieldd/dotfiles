c8ed28ddfe2c7c2f29e199a466d37db5ef2d7e56
Ariel Diaz <appa232@gmail.com>
2024-04-01 18:12:35 +0100

    Added toggleterm keymaps

--- lua/default/plugins/toggleterm.lua
+++ lua/default/plugins/toggleterm.lua
@@ -3,7 +3,10 @@ return {
 	version = "*",
 	config = function()
 		require("toggleterm").setup({
-			open_mapping = [[<c-\>]],
+			open_mapping = [[<leader>t]],
 		})
+
+		vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
+		vim.keymap.set("t", "jk", [[<C-\><C-n>]])
 	end,
 }
