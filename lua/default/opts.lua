0345cffee1c948cc49a3edd4526f61bacad3b697
Ariel Diaz <appa232@gmail.com>
2024-04-01 02:36:39 +0100

    Changed column width coloring to 80.

--- lua/default/opts.lua
+++ lua/default/opts.lua
@@ -31,7 +31,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "100"
+vim.opt.colorcolumn = "80"
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
