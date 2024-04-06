9b6239f825ea702e3703e50137fcd11887e2fbf1
Ariel Diaz <appa232@gmail.com>
2024-04-06 18:56:05 +0100

    Textobjects new bindings.

--- lua/default/plugins/treesitter-setup/textobjects.lua
+++ lua/default/plugins/treesitter-setup/textobjects.lua
@@ -4,8 +4,17 @@ return {
 		lookahead = true,
 
 		keymaps = {
-			["af"] = "@function.outer",
-			["if"] = "@function.inner",
+			["a="] = { query = "@assignment.outer", desc = "Select [a]round assignment" },
+			["i="] = { query = "@assignment.inner", desc = "Select [i]nside assignment" },
+			["l="] = { query = "@assignment.lhs", desc = "Select assignment [l]hs" },
+			["r="] = { query = "@assignment.rhs", desc = "Select assignment [r]hs" },
+
+			["aa"] = { query = "@parameter.outer", desc = "Select [a]round [a]rgument" },
+			["ia"] = { query = "@parameter.inner", desc = "Select [i]nside [a]rgument" },
+
+			["af"] = { query = "@function.outer", desc = "Select [a]round [f]unction" },
+			["if"] = { query = "@function.inner", desc = "Select [i]nside [f]unction" },
+
 			["ac"] = "@class.outer",
 			["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
 			["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
@@ -20,31 +29,28 @@ return {
 		enable = true,
 		set_jumps = true, -- whether to set jumps in the jumplist
 		goto_next_start = {
-			["]f"] = { query = "@function.outer", desc = "Nwxt function start" },
-			["]]"] = { query = "@class.outer", desc = "Next class start" },
-			["]b"] = { query = "@block.outer", desc = "Next block start" },
-			--
-			-- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
-			["]o"] = { query = "@loop.*", desc = "Next loop" },
-			-- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-			--
-			["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+			["]f"] = { query = "@function.outer", desc = "Next [f]unction start" },
+			["]k"] = { query = "@block.outer", desc = "Next bloc[k] start" },
+			["]a"] = { query = "@parameter.inner", desc = "Next [a]rgument start" },
+
+			["]o"] = { query = "@loop.*", desc = "Next lo[o]p" },
+			["]s"] = { query = "@scope", query_group = "locals", desc = "Next [s]cope" },
 			["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
 		},
 		goto_next_end = {
-			["]F"] = { query = "@function.outer", desc = "Next function end" },
-			["]["] = { query = "@class.outer", desc = "Next class end" },
-			["]B"] = { query = "@block.outer", desc = "Next block end" },
+			["]F"] = { query = "@function.outer", desc = "Next [f]unction end" },
+			["]K"] = { query = "@block.outer", desc = "Next bloc[k] end" },
+			["]A"] = { query = "@parameter.inner", desc = "Next [a]rgument end" },
 		},
 		goto_previous_start = {
-			["[f"] = { query = "@function.outer", desc = "Previous function start" },
-			["[["] = { query = "@class.outer", desc = "Previous class end" },
-			["[b"] = { query = "@block.outer", desc = "Previous block start" },
+			["[f"] = { query = "@function.outer", desc = "Previous [f]unction start" },
+			["[k"] = { query = "@block.outer", desc = "Previous bloc[k] start" },
+			["[a"] = { query = "@parameter.inner", desc = "Previous [a]rgument start" },
 		},
 		goto_previous_end = {
-			["[F"] = { query = "@function.outer", desc = "Previous function end" },
-			["[]"] = { query = "@class.outer", desc = "Previous class end" },
-			["[B"] = { query = "@block.outer", desc = "Previous block end" },
+			["[F"] = { query = "@function.outer", desc = "Previous [f]unction end" },
+			["[K"] = { query = "@block.outer", desc = "Previous bloc[k] end" },
+			["[A"] = { query = "@parameter.inner", desc = "Previous [a]rgument end" },
 		},
 	},
 	swap = {
