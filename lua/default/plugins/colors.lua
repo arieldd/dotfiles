b2bca30121d866253ff3b83650c19dc738e6ec21
Ariel Diaz <appa232@gmail.com>
2024-04-08 02:34:50 +0100

    Fixed colors and lualine Noice interop. Back to Catppuccin.

--- lua/default/plugins/colors.lua
+++ lua/default/plugins/colors.lua
@@ -1,35 +1,19 @@
-local color_themes = {
-	["catppuccin"] = { "catppuccin/nvim", name = "catppuccin" },
-	["rose-pine"] = { "rose-pine/neovim", name = "rose-pine" },
-	["gruvbox"] = { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
-	["gruvbox-material"] = { "f4z3r/gruvbox-material.nvim", name = "gruvbox-material" },
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"f4z3r/gruvbox-material.nvim",
+		name = "gruvbox-material",
+		-- priority = 1000,
+		-- config = function()
+		-- 	vim.g.gruvbox_material_transparent_background = 1
+		-- 	require("gruvbox-material").setup()
+		-- end,
+	},
 }
-
-local function setTheme(theme_name)
-	local theme = color_themes[theme_name]
-
-	theme.priority = 1000
-	theme.opts = {
-		integrations = {
-			telescope = true,
-			mason = true,
-			neotest = true,
-		},
-	}
-	theme.config = function(_, opts)
-		require(theme_name).setup(opts)
-		vim.cmd.colorscheme(theme_name)
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-	end
-end
-
-setTheme("gruvbox-material")
-
-local plugins = {}
-
-for _, v in pairs(color_themes) do
-	table.insert(plugins, v)
-end
-
-return plugins
--- lua/default/plugins/lualine.lua
+++ lua/default/plugins/lualine.lua
@@ -2,14 +2,30 @@ return { -- Status line below
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local noice = require("noice")
+
 		require("lualine").setup({
-			options = { theme = "gruvbox-material" },
-			-- options = { theme = "catppuccin" },
+			-- options = { theme = "gruvbox-material" },
+			options = { theme = "catppuccin" },
 			sections = {
 				lualine_x = {
 					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
+						noice.api.status.message.get_hl,
+						cond = noice.api.status.message.has,
+					},
+					{
+						noice.api.status.command.get,
+						cond = noice.api.status.command.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						noice.api.status.mode.get,
+						cond = noice.api.status.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						noice.api.status.search.get,
+						cond = noice.api.status.search.has,
 						color = { fg = "#ff9e64" },
 					},
 				},
