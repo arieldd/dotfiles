72f7e99259cc1ea71d7d734c3b4de84c6f6520ed
Ariel Diaz <appa232@gmail.com>
2024-04-12 02:44:03 +0100

    Recommended noice setup.

--- lua/default/plugins/noice.lua
+++ lua/default/plugins/noice.lua
@@ -6,7 +6,19 @@ return {
 	},
 	enabled = true,
 	config = function()
-		require("noice").setup({})
+		require("noice").setup({
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			presets = {
+				lsp_doc_border = true,
+			},
+		})
 		require("notify").setup({
 			render = "wrapped-compact",
 			stages = "static",
