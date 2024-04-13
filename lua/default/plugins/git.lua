7b7fbe8943000cdc34885c4cf9d4432e060eb090
Ariel Diaz <appa232@gmail.com>
2024-04-13 23:57:59 +0100

    Proper LG keymap desc.

--- lua/default/plugins/git.lua
+++ lua/default/plugins/git.lua
@@ -19,7 +19,7 @@ return {
 		-- setting the keybinding for LazyGit with 'keys' is recommended in
 		-- order to load the plugin when the command is run for the first time
 		keys = {
-			{ "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit", silent = true } },
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 		},
 	},
 }
