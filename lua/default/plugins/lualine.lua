38e9d7bae956932419de4dc7bd9e535ab853d574
Ariel Diaz <arieldd@amazon.co.uk>
2024-04-23 15:59:50 +0100

    Removed Trouble symbols from status line. Added file path and status.

--- lua/default/plugins/lualine.lua
+++ lua/default/plugins/lualine.lua
@@ -3,21 +3,17 @@ return { -- Status line below
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local noice = require("noice")
-
-		local symbols = require("trouble").statusline({
-			mode = "lsp_document_symbols",
-			groups = {},
-			-- max_items = 3,
-			title = false,
-			filter = { range = true },
-			format = "{kind_icon}{symbol.name}",
-		})
-
 		local lualine = require("lualine")
 		lualine.setup({
-			-- options = { theme = "gruvbox-material" },
 			options = { theme = "catppuccin" },
 			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true,
+						path = 1,
+					},
+				},
 				lualine_x = {
 					{
 						noice.api.status.message.get_hl,
@@ -37,12 +33,6 @@ return { -- Status line below
 						noice.api.status.search.get,
 						cond = noice.api.status.search.has,
 						color = { fg = "#ff9e64" },
-					},
-				},
-				lualine_c = {
-					{
-						symbols.get,
-						cond = symbols.has,
 					},
 				},
 			},
