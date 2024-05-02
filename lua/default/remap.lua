e3afd9bbf926c74b21b83f0792660fb3ffa17ba9
Ariel Diaz <appa232@gmail.com>
2024-05-02 23:34:52 +0100

    Added keybind for replaying macros.

--- lua/default/remap.lua
+++ lua/default/remap.lua
@@ -34,3 +34,6 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "Q", "@qj", { desc = "Play macro recorded at [Q] register" })
+vim.keymap.set("x", "Q", ":norm @q<CR>", { desc = "Play macro recorded at [Q] register on selection" })
