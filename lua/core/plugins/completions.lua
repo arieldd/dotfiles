0af0aa85d2270d94b4b7caff4482fab061b810d6
Ariel Diaz <appa232@gmail.com>
2024-05-16 20:53:27 +0100

    Added LspKind and trying new snippet keymaps.

--- lua/core/plugins/completions.lua
+++ lua/core/plugins/completions.lua
@@ -23,14 +23,17 @@ return {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"saadparwaiz1/cmp_luasnip",
+		"onsails/lspkind.nvim",
 	},
 	config = function()
-		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 		luasnip.config.setup({})
-
 		require("luasnip.loaders.from_lua").lazy_load({ paths = { "./lua/core/plugins/snippets" } })
 
+		local lspkind = require("lspkind")
+		lspkind.init({})
+
+		local cmp = require("cmp")
 		cmp.setup({
 			snippet = {
 				expand = function(args)
@@ -51,28 +54,22 @@ return {
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
 				["<C-Space>"] = cmp.mapping.complete({}),
 
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
+				["<C-k>"] = cmp.mapping(function()
 					if luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
 					end
 				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
+				["<C-j>"] = cmp.mapping(function()
 					if luasnip.locally_jumpable(-1) then
 						luasnip.jump(-1)
 					end
 				end, { "i", "s" }),
 			}),
+			formatting = {
+				format = lspkind.cmp_format({}),
+			},
 			sources = {
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
 				{ name = "buffer" },
 				{ name = "path" },
 			},
