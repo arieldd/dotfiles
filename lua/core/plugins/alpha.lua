36a9c31a5f50a3d345cb2bb1075a8665845ce565
Ariel Diaz <appa232@gmail.com>
2024-05-20 23:17:25 +0100

    Reorganise Dashboard screen.

--- lua/core/plugins/alpha.lua
+++ lua/core/plugins/alpha.lua
@@ -52,15 +52,14 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			-- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
+			dashboard.button("s", "󰈚   Restore session", ":lua require('persistence').load()<CR>"),
 			dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
 			dashboard.button("f", "   Find file", ":cd $HOME/.config | Telescope find_files<CR>"),
-			dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
+			dashboard.button("g", "󰱼   Live grep", ":Telescope live_grep<CR>"),
 			dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
-			dashboard.button("s", "   Restore session", ":lua require('persistence').load()<CR>"),
-			dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
 			dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
 			dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
+			dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
 			dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
 		}
 
