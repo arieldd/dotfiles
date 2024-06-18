829bb99798f8298c0148bea50ebfc46506a010b5
Ariel Diaz <arieldd@amazon.co.uk>
2024-06-18 13:32:21 +0100

    Use everforest theme.

--- lua/core/plugins/colors.lua
+++ lua/core/plugins/colors.lua
@@ -2,23 +2,23 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				integrations = {
-					harpoon = true,
-					mason = true,
-					noice = true,
-					notify = true,
-				},
-				custom_highlights = function(colors)
-					return {
-						TelescopePromptBorder = { fg = colors.peach },
-					}
-				end,
-			})
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
+		-- priority = 1000,
+		-- config = function()
+		-- require("catppuccin").setup({
+		-- 	integrations = {
+		-- 		harpoon = true,
+		-- 		mason = true,
+		-- 		noice = true,
+		-- 		notify = true,
+		-- 	},
+		-- 	custom_highlights = function(colors)
+		-- 		return {
+		-- 			TelescopePromptBorder = { fg = colors.peach },
+		-- 		}
+		-- 	end,
+		-- })
+		-- vim.cmd.colorscheme("catppuccin-mocha")
+		-- end,
 	},
 	{
 		"sainnhe/gruvbox-material",
@@ -33,9 +33,10 @@ return {
 	{
 		"sainnhe/everforest",
 		name = "everforest",
-		-- priority = 1000,
-		-- config = function()
-		-- 	vim.cmd.colorscheme("everforest")
-		-- end,
+		priority = 1000,
+		config = function()
+			vim.g.everforest_enable_italic = true
+			vim.cmd.colorscheme("everforest")
+		end,
 	},
 }
