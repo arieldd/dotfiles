3bf551a18152166cfd8fdfed511e5b045cc40a6e
Ariel Diaz <appa232@gmail.com>
2024-10-26 13:27:14 +0100

    Contrain oil cursor + demap C-p.

--- lua/core/plugins/oil.lua
+++ lua/core/plugins/oil.lua
@@ -3,14 +3,18 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
+			default_file_explorer = true,
 			columns = {
 				"icon",
 				"permissions",
 				"size",
 			},
+			skip_confirm_for_simple_edits = true,
 			keymaps = {
 				["<C-h>"] = false,
+				["<C-p>"] = false,
 			},
+			constrain_cursor = "name",
 		})
 
 		vim.keymap.set("n", "-", ":Oil<CR>", { silent = true, desc = "Open parent directory" })
