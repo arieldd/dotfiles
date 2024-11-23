a5efb3fb9b1ac25d492ea9cf7f0e563e0b2aca85
Ariel Diaz <appa232@gmail.com>
2024-11-23 23:37:05 +0000

    C++ AoC snippet.

---
+++ lua/core/plugins/snippets/cpp.lua
@@ -0,0 +1,36 @@
+local ls = require("luasnip")
+
+return {
+	ls.snippet("aoc", {
+		ls.text_node({
+			[[#include <bits/stdc++.h>]],
+			[[#include <cassert>]],
+			[[using namespace std;]],
+			[[]],
+			[[#define ll long long]],
+			[[#define arr array]],
+			[[]],
+			[[vector<string> read_input(const string &filename) {]],
+			[[  vector<string> lines{};]],
+			[[  ifstream fs(filename);]],
+			[[]],
+			[[  string line;]],
+			[[  while (getline(fs, line))]],
+			[[    lines.push_back(line);]],
+			[[  return lines;]],
+			[[}]],
+			[[]],
+			[[int part1(const vector<string> &lines) { return 0; }]],
+			[[]],
+			[[int part2(const vector<string> &lines) { return 0; }]],
+			[[]],
+			[[int main(int argc, char *argv[]) {]],
+			[[  assert(argc > 1 && "Need some input brotha\n");]],
+			[[  auto lines = read_input(argv[1]);]],
+			[[  for (auto const &l : lines)]],
+			[[    cout << l << '\n';]],
+			[[  return 0;]],
+			[[}]],
+		}),
+	}),
+}
