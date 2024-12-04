3b21dd61061e9c2454a7f67e19ba8abdb94a0e7c
Ariel Diaz De Armas <arieldd@amazon.co.uk>
2024-12-04 08:49:32 +0000

    Added Make snippet for AoC.

---
+++ lua/core/plugins/snippets/make.lua
@@ -0,0 +1,16 @@
+local ls = require("luasnip")
+
+return {
+	ls.snippet("aoc", {
+		ls.text_node({
+			[[run: solution]],
+			[[	./solution input.txt]],
+			[[]],
+			[[test: solution]],
+			[[	./solution test.txt]],
+			[[]],
+			[[solution: solution.cpp]],
+			[[	clang++ -std=c++20 -o solution solution.cpp]],
+		}),
+	}),
+}
