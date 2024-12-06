02b9c477ca4c83c2efc9abef5e543e838b2efe7e
Ariel Diaz De Armas <arieldd@amazon.co.uk>
2024-12-06 19:52:32 +0000

    Added Snacks from Folke for LazyGit float.

--- lua/core/plugins/git.lua
+++ lua/core/plugins/git.lua
@@ -4,22 +4,17 @@ return {
 		enabled = true,
 	},
 	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
+		"folke/snacks.nvim",
+		opts = {
+			lazygit = {},
 		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
-		keys = {
-			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
+		config = function()
+			vim.keymap.set(
+				"n",
+				"<leader>lg",
+				":lua Snacks.lazygit.open()<CR>",
+				{ desc = "Open LazyGit from Snacks", silent = true }
+			)
+		end,
 	},
 }
