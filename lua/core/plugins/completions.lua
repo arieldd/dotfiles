d8527631b174cc6ca0abf15108a4dfe1f825ae11
Ariel Diaz <appa232@gmail.com>
2025-01-08 10:23:18 +0000

    Add blink changes to snippets.

--- lua/core/plugins/completions.lua
+++ lua/core/plugins/completions.lua
@@ -31,22 +31,9 @@ return {
 					},
 				},
 			},
+			snippets = { preset = "luasnip" },
 			sources = {
-				default = { "lsp", "path", "buffer", "luasnip" },
-			},
-			snippets = {
-				expand = function(snippet)
-					require("luasnip").lsp_expand(snippet)
-				end,
-				active = function(filter)
-					if filter and filter.direction then
-						return require("luasnip").jumpable(filter.direction)
-					end
-					return require("luasnip").in_snippet()
-				end,
-				jump = function(direction)
-					require("luasnip").jump(direction)
-				end,
+				default = { "lsp", "path", "snippets", "buffer" },
 			},
 		},
 	},
